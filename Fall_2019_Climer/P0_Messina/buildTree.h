f9f82aea00cd9adf0c9c5252cf23badfae437afb
Andrew Messina <andrewdmessina@gmail.com>
2019-08-31 13:26:07 +0000

    Updated introductory identifying info

--- Fall_2019_Climer/P0_Messina/buildTree.h
+++ Fall_2019_Climer/P0_Messina/buildTree.h
@@ -1,3 +1,7 @@
+// Andrew D Messina
+// andrewdmessina@gmail.com
+// UMSL CS4280_002 Fall 2019 Sharlee Climer
+
 #ifndefine BUILDTREE_H
 #define BUILDTREE_H
 
--- Fall_2019_Climer/P0_Messina/traversals.h
+++ Fall_2019_Climer/P0_Messina/traversals.h
@@ -1,3 +1,7 @@
+// Andrew D Messina
+// andrewdmessina@gmail.com
+// UMSL CS4280_002 Fall 2019 Sharlee Climer
+
 #ifndefine TRAVERSALS_H
 #define TRAVERSALS_H
 
