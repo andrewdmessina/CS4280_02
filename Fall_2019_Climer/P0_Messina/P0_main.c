62cf75d73cd8172b5f26daba626210afa20739f1
Andrew Messina <adm244@mail.umsl.edu>
2019-09-12 10:27:04 -0500

    Added node.h, working on makeP0

--- Fall_2019_Climer/P0_Messina/P0_main.c
+++ Fall_2019_Climer/P0_Messina/P0_main.c
@@ -2,14 +2,14 @@
 // andrewdmessina@gmail.com
 // UMSL CS4280_002 Fall 2019 Sharlee Climer
 
-#include traversals.h
-#include buildTree.h
+#include <stdio.h>
+#include <traversals.h>
+#include <buildTree.h>
 
 
 int main(){
 
-// stuff
-
+printf('Hellow World');
 
 return 0;
 }
