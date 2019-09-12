92c242b562269ccdf57fa50cee88439616f31bac
Andrew Messina <adm244@mail.umsl.edu>
2019-09-12 11:33:53 -0500

    Fixed git merge

--- Fall_2019_Climer/P0_Messina/buildTree.h
+++ Fall_2019_Climer/P0_Messina/buildTree.h
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-#ifndef BUILDTREE_H
-=======
 // Andrew D Messina
 // andrewdmessina@gmail.com
 // UMSL CS4280_002 Fall 2019 Sharlee Climer
-
-#ifndefine BUILDTREE_H
->>>>>>> f9f82aea00cd9adf0c9c5252cf23badfae437afb
+#ifndef BUILDTREE_H
 #define BUILDTREE_H
 
 // stuff
--- Fall_2019_Climer/P0_Messina/traversals.h
+++ Fall_2019_Climer/P0_Messina/traversals.h
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-#ifndef TRAVERSALS_H
-=======
 // Andrew D Messina
 // andrewdmessina@gmail.com
 // UMSL CS4280_002 Fall 2019 Sharlee Climer
 
-#ifndefine TRAVERSALS_H
->>>>>>> f9f82aea00cd9adf0c9c5252cf23badfae437afb
+#ifndef TRAVERSALS_H
 #define TRAVERSALS_H
-
 
 // stuff
 
